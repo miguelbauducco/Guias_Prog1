5c9675ce2097ac772eac9111bc0240438aa66de4
miguelbauducco <miguel.bau14@gmail.com>
2024-06-04 18:12:48 -0300

    ejercicio guia 8

--- ControlDeAcceso/ControlDeAcceso/Program.cs
+++ ControlDeAcceso/ControlDeAcceso/Program.cs
@@ -1,144 +1,86 @@
-﻿}using System;
+﻿using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
-namespace Controldeacceso
+namespace Guia8
 {
     internal class Program
     {
 
-        static double Cobro(int vehiculo, int cantVehiculo)
+        public static int BusquedaSecuencial(int[] listado, int valor)
         {
-            double valorBase = 0;
-
-            switch (vehiculo)
+            int i = 0;
+            int pos = -1;
+            while ((pos == -1) && (i < listado.Length))
             {
-                case 1:
-                    valorBase = 100;
-                    break;
-
-                case 2:
-                    valorBase = cantVehiculo * 800;
-                    break;
-
-                case 3:
-                    valorBase = cantVehiculo * 1000;
-                    break;
-
-                case 4:
-                    valorBase = cantVehiculo * 1500;
-                    break;
-
-                case 5:
-                    valorBase = cantVehiculo * 5000;
-                    break;
-
-                case 6:
-                    valorBase = cantVehiculo * 1200;
-                    break;
-
-                default:
-                    Console.WriteLine("ERROR: Opcion no valida");
-                    break;
+                if (listado[i] == valor)
+                {
+                    pos = i;
+                }
+                i++;
             }
-
-            return valorBase;
-        }
-
-
-        static double Porc(int cantDias, double valorBase)
-        {
-            double porcentaje = 0;
-
-            switch (cantDias)
-            {
-                case 1:
-                    porcentaje = valorBase * 1.00;
-                    break;
-
-                case 2:
-                    porcentaje = valorBase * 1.20;
-                    break;
-
-                case 3:
-                    porcentaje = valorBase * 2.20;
-                    break;
-
-                case 4:
-                    porcentaje = valorBase * 3.20;
-                    break;
-
-
-            }
-
-            if (cantDias >= 5 && cantDias <= 10)
-            {
-                porcentaje = valorBase * 3.80;
-
-            }
-
-            return porcentaje;
-
-        }
-
-        static double Impuestos(double iva, double impBoludo, double porcentaje)
-        {
-            double valorFinal = 0;
-
-            valorFinal = porcentaje + ((porcentaje * iva) * impBoludo);
-
-            return valorFinal;
-
+            return pos;
         }
 
 
 
         static void Main(string[] args)
         {
-            double rec = 0;
-            double mayorTicket = 0;
-            int ticket, cantVehiculo, tipoVehiculo, cantDias;
-            int total;
+            int opcion, aux;
+            int[] listado;
 
-            Console.WriteLine("Ticket valido? 1.Si 0.No");
-            ticket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Defina la cantida de numeros a ingresar: ");
+            listado = new int[Convert.ToInt32(Console.ReadLine())];
 
-            if (ticket == 0)
+            for (int i = 0; i < listado.Length; i++)
             {
-                Console.WriteLine("Ingrese tipo de vehiculo 1.NINGUNO 2.MOTO 3.AUTO 4.CAMIONETA 5.BUGY 6.VEHICULO NAUTICO 0.SALIR");
-                tipoVehiculo = Convert.ToInt32(Console.ReadLine());
-
-                while (tipoVehiculo != 0)
-                {
-                    // metodo 1
-                    Console.WriteLine("Ingrese cantidad de vehiculos: "); 
-                    cantVehiculo = Convert.ToInt32(Console.ReadLine());
-                    //Cobro(tipoVehiculo, cantVehiculo);
-
-                    // metodo 2
-                    Console.WriteLine("Ingrese cantidad de dias: ");
-                    cantDias = Convert.ToInt32(Console.ReadLine());
-                    //Porc(cantDias, Cobro(tipoVehiculo, cantVehiculo));
-
-                    // metodo 3
-                    Console.WriteLine("COSTO FINAL: ");
-                    Console.WriteLine(Impuestos(0.21, 0.15, Porc(cantDias, Cobro(tipoVehiculo, cantVehiculo))));
-
-
-                    Console.WriteLine("Ingrese tipo de vehiculo 1.NINGUNO 2.MOTO 3.AUTO 4.CAMIONETA 5.BUGY 6.VEHICULO NAUTICO 0.SALIR");
-                    tipoVehiculo = Convert.ToInt32(Console.ReadLine());
-
-                }
-
-            }
-            else if (ticket == 1)
-            {
-                Console.WriteLine("Ticket valido.");
+                Console.WriteLine("Ingrese numero en la posicion {0}: ", i);
+                listado[i] = Convert.ToInt32(Console.ReadLine());
             }
 
+            Console.WriteLine("Elija una opcion:\n 1.MOSTRAR LISTADO ORDENADO \n 2.BUSCAR UN ELEMENTO DEL LISTADO");
+            opcion = Convert.ToInt32(Console.ReadLine());
 
+            switch (opcion)
+            {
 
+                case 1:
+                    Console.WriteLine("LISTA ORDENADA: ");
+                    for (int i = 0; i < listado.Length - 1; i++)
+                    {
+                        for (int j = i+1; j< listado.Length; j++)
+                        {
+                            if (listado[i] > listado[j])
+                            {
+                                aux = listado[i];
+                                listado[i] = listado[j];
+                                listado[j] = aux;
+          
+                            }
+                        }
+                    }
+                    for (int i = 0; i < listado.Length; i++)
+                    {
+                        Console.WriteLine(listado[i]);
+                    }
+                break;
+
+                case 2:
+                    Console.WriteLine("¿Que valor desea encontrar?");
+                    int busqueda = Convert.ToInt32(Console.ReadLine());
+                    if (BusquedaSecuencial(listado, busqueda) == -1)
+                    {
+                        Console.WriteLine("El dato no se encontro");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("EL RESULTADO SE ENCUENTRA EN LA POSICION: "+BusquedaSecuencial(listado, busqueda));
+                    }
+                break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
