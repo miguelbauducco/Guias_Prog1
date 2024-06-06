2b6891ff4d20444595809b8eb79ed04eac405e07
miguelbauducco <miguel.bau14@gmail.com>
2024-06-06 18:16:37 -0300

    Ejercicio 8 arreglos paralelos

--- ControlDeAcceso/ControlDeAcceso/Program.cs
+++ ControlDeAcceso/ControlDeAcceso/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Security.AccessControl;
 
 namespace Guia8
 {
@@ -23,62 +25,182 @@ namespace Guia8
             return pos;
         }
 
+        public static void OrdenamientoBurbuja(int[] lista) 
+        {
+        
+            int aux, i, j;
+            for (i = 0; i < lista.Length - 1; i++)
+            {
+                for (j = 0;j<lista.Length; j++)
+                {
+                    if (lista[i] > lista[j])
+                    {
+                        aux = lista[i];
+                        lista[i] = lista[j];
+                        lista[j] = aux;
+                    }
+                }
+            }
+        }
+
 
 
         static void Main(string[] args)
         {
-            int opcion, aux;
-            int[] listado;
+            int opcion, busqueda, pos;
 
-            Console.WriteLine("Defina la cantida de numeros a ingresar: ");
-            listado = new int[Convert.ToInt32(Console.ReadLine())];
+            int c = 0;
+            string alumnoNotaMayor="";
+            string alumnoNotaMenor = "";
+            float promedio = 0;
+            float mayor = 0;
+            float menor = 111111;
 
-            for (int i = 0; i < listado.Length; i++)
+            string[] nombres;
+            nombres = new string[3];
+            int[] nroLibreta;
+            nroLibreta = new int[3];
+            int[] notas;
+            notas = new int[3];
+
+            string[] alumnosProm;
+            int[] nroLibretaProm;
+            int[] notasProm;
+
+            Console.WriteLine("Ingrese nombres de alumnos: \n");
+            for (int i = 0; i < nombres.Length; i++)
             {
-                Console.WriteLine("Ingrese numero en la posicion {0}: ", i);
-                listado[i] = Convert.ToInt32(Console.ReadLine());
+                nombres[i] = Console.ReadLine();
             }
 
-            Console.WriteLine("Elija una opcion:\n 1.MOSTRAR LISTADO ORDENADO \n 2.BUSCAR UN ELEMENTO DEL LISTADO");
+            Console.WriteLine("\nIngrese numero de libreta de alumnos: \n");
+            for (int i = 0; i < nroLibreta.Length; i++)
+            {
+                nroLibreta[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nIngrese las notas de alumnos: \n");
+            for (int i = 0; i < notas.Length; i++)
+            {
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("\nPromedio de notas: \n");
+            for (int i = 0; i < notas.Length; i++)
+            {
+                promedio = (promedio + notas[i]);
+            }
+            promedio = promedio / nombres.Length;
+            Console.WriteLine(promedio);
+
+            for (int i = 0; notas.Length > i; i++)
+            {
+                if (notas[i] > promedio)
+                {
+
+                    c++;
+
+                }
+            }
+
+            alumnosProm = new string[c];
+            nroLibretaProm = new int[c];
+            notasProm = new int[c];
+
+            for (int i = 0; i < alumnosProm.Length; i++)
+            {
+
+                alumnosProm[i] = nombres[i];
+
+            }
+
+            for (int i = 0; i < nroLibretaProm.Length; i++)
+            {
+
+                nroLibretaProm[i] = nroLibreta[i];
+
+            }
+
+            for (int i = 0; i < notasProm.Length; i++)
+            {
+
+                notasProm[i] = notas[i];
+
+            }
+
+
+
+
+            for (int i = 0; alumnosProm.Length > i; i++)
+            {
+                Console.WriteLine("\nAlumnos que superaron el promedio:\n" + alumnosProm[i]);
+            }
+
+            for (int i = 0; nroLibretaProm.Length > i; i++)
+            {
+                Console.WriteLine("\nNro de libreta que superaron el promedio:\n" + nroLibretaProm[i]);
+            }
+
+            for (int i = 0; notasProm.Length > i; i++)
+            {
+                Console.WriteLine("\nNotas de alumnos que superaron el promedio:\n" + notasProm[i]);
+            }
+
+            
+            OrdenamientoBurbuja(nroLibretaProm);
+
+            Console.WriteLine("\nPresione 1 si desea ordenar la lista de alumnos por numero libreta de mayor a menor.\nPresione 2 para buscar por libreta de alumno\nCualquier boton para salir.");
             opcion = Convert.ToInt32(Console.ReadLine());
 
             switch (opcion)
             {
-
                 case 1:
-                    Console.WriteLine("LISTA ORDENADA: ");
-                    for (int i = 0; i < listado.Length - 1; i++)
+                    
+                    Console.WriteLine("\nLISTA DE ALUMNOS ORDENADA: \n");
+                    for (int i = 0; i < nroLibretaProm.Length; i++)
                     {
-                        for (int j = i+1; j< listado.Length; j++)
+
+                        Console.WriteLine(nroLibreta[i]);
+                        Console.WriteLine(alumnosProm[i]);
+                        Console.WriteLine(notasProm[i]);
+
+                        if (notasProm[i] > mayor)
                         {
-                            if (listado[i] > listado[j])
-                            {
-                                aux = listado[i];
-                                listado[i] = listado[j];
-                                listado[j] = aux;
-          
-                            }
+                            mayor = notasProm[i];
+                            alumnoNotaMayor = alumnosProm[i];
                         }
+
+                        if (notasProm[i] < menor)
+                        {
+                            menor = notasProm[i];
+                            alumnoNotaMenor = alumnosProm[i];
+                        }
+
                     }
-                    for (int i = 0; i < listado.Length; i++)
-                    {
-                        Console.WriteLine(listado[i]);
-                    }
+
+                    Console.WriteLine("\n Alumno de mayor nota: " + alumnoNotaMayor);
+                    Console.WriteLine("\n Alumno de menor nota: " + alumnoNotaMenor);
+
                 break;
 
                 case 2:
-                    Console.WriteLine("¿Que valor desea encontrar?");
-                    int busqueda = Convert.ToInt32(Console.ReadLine());
-                    if (BusquedaSecuencial(listado, busqueda) == -1)
-                    {
-                        Console.WriteLine("El dato no se encontro");
-                    }
-                    else 
-                    {
-                        Console.WriteLine("EL RESULTADO SE ENCUENTRA EN LA POSICION: "+BusquedaSecuencial(listado, busqueda));
-                    }
+                    Console.Clear();
+                    Console.WriteLine("Ingrese numero de libreta de alumno para su busqueda: \n");
+                    busqueda = Convert.ToInt32(Console.ReadLine());
+                    pos = (BusquedaSecuencial(nroLibreta, busqueda));
+                    Console.WriteLine("\nNumero de libreta:\n");
+                    Console.WriteLine(nroLibreta[pos]);
+                    Console.WriteLine("\nNombre:\n");
+                    Console.WriteLine(nombres[pos]);
+                    Console.WriteLine("\nNota:\n");
+                    Console.WriteLine(notas[pos]);
                 break;
             }
+
+
+
 
             Console.ReadKey();
         }
